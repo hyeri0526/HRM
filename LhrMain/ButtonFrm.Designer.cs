9b2f87ad2b8c9870bf5f635632f26f64a8d0401d
Lee Hyeri <80086193+hyeri0526@users.noreply.github.com>
2023-05-08 11:11:56 +0900

    Add files via upload

---
+++ LhrMain/ButtonFrm.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace LhrMain
+{
+    partial class ButtonFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ButtonFrm));
+            this.btn_search = new System.Windows.Forms.Button();
+            this.btn_print = new System.Windows.Forms.Button();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_search
+            // 
+            this.btn_search.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_search.Image = ((System.Drawing.Image)(resources.GetObject("btn_search.Image")));
+            this.btn_search.Location = new System.Drawing.Point(8, 7);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(32, 32);
+            this.btn_search.TabIndex = 8;
+            this.btn_search.Tag = "BtnSearch_Click";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.BtnFunc_Click);
+            // 
+            // btn_print
+            // 
+            this.btn_print.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_print.Image = ((System.Drawing.Image)(resources.GetObject("btn_print.Image")));
+            this.btn_print.Location = new System.Drawing.Point(230, 7);
+            this.btn_print.Name = "btn_print";
+            this.btn_print.Size = new System.Drawing.Size(32, 32);
+            this.btn_print.TabIndex = 14;
+            this.btn_print.Tag = "BtnPrint_Click";
+            this.btn_print.UseVisualStyleBackColor = true;
+            this.btn_print.Click += new System.EventHandler(this.BtnFunc_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_close.Image = ((System.Drawing.Image)(resources.GetObject("btn_close.Image")));
+            this.btn_close.Location = new System.Drawing.Point(266, 7);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(32, 32);
+            this.btn_close.TabIndex = 15;
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.BtnFunc_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_cancel.Image = ((System.Drawing.Image)(resources.GetObject("btn_cancel.Image")));
+            this.btn_cancel.Location = new System.Drawing.Point(193, 7);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(32, 32);
+            this.btn_cancel.TabIndex = 13;
+            this.btn_cancel.Tag = "BtnCancel_Click";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.BtnFunc_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_save.Image = ((System.Drawing.Image)(resources.GetObject("btn_save.Image")));
+            this.btn_save.Location = new System.Drawing.Point(156, 7);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(32, 32);
+            this.btn_save.TabIndex = 12;
+            this.btn_save.Tag = "BtnSave_Click";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.BtnFunc_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_delete.Image = ((System.Drawing.Image)(resources.GetObject("btn_delete.Image")));
+            this.btn_delete.Location = new System.Drawing.Point(119, 7);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(32, 32);
+            this.btn_delete.TabIndex = 11;
+            this.btn_delete.Tag = "BtnDelete_Click";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.BtnFunc_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_update.Image = ((System.Drawing.Image)(resources.GetObject("btn_update.Image")));
+            this.btn_update.Location = new System.Drawing.Point(82, 7);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(32, 32);
+            this.btn_update.TabIndex = 10;
+            this.btn_update.Tag = "BtnUpdate_Click";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.BtnFunc_Click);
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_insert.Image = ((System.Drawing.Image)(resources.GetObject("btn_insert.Image")));
+            this.btn_insert.Location = new System.Drawing.Point(45, 7);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(32, 32);
+            this.btn_insert.TabIndex = 9;
+            this.btn_insert.Tag = "BtnInsert_Click";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            this.btn_insert.Click += new System.EventHandler(this.BtnFunc_Click);
+            // 
+            // ButtonFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 48);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.btn_print);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_insert);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ButtonFrm";
+            this.Text = "ButtonFrm";
+            this.Load += new System.EventHandler(this.ButtonFrm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Button btn_print;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button btn_insert;
+    }
+}
---
+++ LhrMain/Launcher.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
+
+namespace LhrMain
+{
+    /// <summary>
+    ///**************************************************************************
+    /// 1. Project        : 교육용 시스템
+    /// 2. 업무분류       : 시스템
+    /// 3. 업무명칭       : 단위업무 런쳐
+    /// 4. 최초작성 정보  : 2023.03.07 / 이혜리
+    /// 5. 최근수정 정보  : 2023.03.07 / 이혜리
+    ///**************************************************************************
+    /// </summary>
+    public partial class Launcher : Form
+    {
+        //************************************************************
+        // 메인메뉴로부터 초기 설정값 넘겨 받기
+        //************************************************************
+        public string LoadingFormName;  // Loading 프로그램 Form Name
+        public string LoadingPgmId;     // Loading 프로그램 Id
+        public string LoadingPgmName;   // Loading 프로그램 이름
+        public string UserId;           // 사용자 ID
+
+        //************************************************************
+        // 로컬변수 선언
+        //************************************************************
+        Button[] button;
+
+        public Launcher()
+        {
+            InitializeComponent();
+        }
+        #region (Event)Form Load 시
+        //**************************************************************************
+        // (Event)Form(론처) Load 시
+        //**************************************************************************
+        private void Launcher_Load(object sender, EventArgs e)
+        {
+            //---2023.03.07-----------------------------------------------------
+            ButtonFrm bf = new ButtonFrm() { TopLevel = false, TopMost = true };
+
+            btn_panel.Controls.Add(bf);
+            bf.Show();
+
+            bf.form_panel = pnl_form;
+            button = bf.button;
+            bf.form_launcher = this;
+            bf.form_load_style = "2";
+            //---2023.03.07-----------------------------------------------------
+
+            Form form = Utility.TryGetFormByName(LoadingFormName);  //이름으로 Form Object 생성
+
+            if (form == null)
+            {
+                MessageBox.Show("해당메뉴 수행 프로그램이 존재하지 않습니다.");
+                return;
+            }
+            form.TopLevel = false;
+            form.FormBorderStyle = FormBorderStyle.None;
+            pnl_form.Controls.Add(form);
+            form.Dock = DockStyle.Fill;
+            this.Text = LoadingPgmName;
+
+            //---Loading Form에 초기값 전달
+            InitialValue_Reflection_to_Form();
+
+            form.Show();
+
+            info_pgmid.Text = LoadingPgmId;
+            info_message.Text = "";
+        }
+        #endregion
+
+        #region 초기설정값 Reflection to Loading Form
+        //**************************************************************************
+        //-- 초기설정값 Reflection to Loading Form
+        //**************************************************************************
+        private void InitialValue_Reflection_to_Form()
+        {
+            Form form = (Form)pnl_form.Controls[0];
+            Type type = form.GetType();
+
+            type.GetProperty("MainBtn")?.SetValue(form, button);
+            type.GetProperty("UserId")?.SetValue(form, UserId);
+            type.GetProperty("Info_Count")?.SetValue(form, info_count);
+            type.GetProperty("Info_Message")?.SetValue(form, info_message);
+            /*--------------------------------------------------------------
+            Button[] button = new Button[7];
+            button[0] = btn_search;
+            button[1] = btn_insert;
+            button[2] = btn_update;
+            button[3] = btn_delete;
+            button[4] = btn_save;
+            button[5] = btn_cancel;
+            button[6] = btn_print;
+
+            PropertyInfo pi = type.GetProperty("MainBtn");
+            if (pi != null) pi.SetValue(form, button);
+
+            pi = type.GetProperty("UserId");
+            if (pi != null) pi.SetValue(form, UserId);
+
+            pi = type.GetProperty("Info_Count");
+            if (pi != null) pi.SetValue(form, info_count);
+
+            pi = type.GetProperty("Info_Message");
+            if (pi != null) pi.SetValue(form, info_message);
+            ------------------------------------------------------------------*/
+
+        }
+        #endregion
+    }
+}
